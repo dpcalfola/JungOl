8d7bf7cb2452791d1af30f59fe04706d4e13acff
Fola Flor <dpcalfola@gmail.com>
2021-12-29 12:06:20 +0900

    Third test commit within IntelliJ

---
+++ src/MyTool/DateRecordApp3_NOW.java
@@ -0,0 +1,36 @@
+package MyTool;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateRecordApp3_NOW {
+
+    static Date today = new Date();
+
+    public static void main(String[] args) throws IOException {
+
+        String EEEE = arrangedFormat("EEEE", "%-9s");
+        String MMMM = arrangedFormat(", MMMM", "%-10s");
+        String dd = arrangedFormat(", dd", "%-3s");
+        String yyyy = arrangedFormat(", yyyy", "%-5s");
+
+        String totalDate = EEEE + MMMM + dd + yyyy;
+
+        FileWriter fw = new FileWriter("./src/myApp/TodayDateRecorde.txt", true);
+
+        fw.write("\n" + totalDate + " : ");
+        fw.close();
+
+        System.out.println("Recorded : " + totalDate);
+
+    }
+
+    private static String arrangedFormat(String pattern, String format) {
+        SimpleDateFormat date = new SimpleDateFormat(pattern);
+        return String.format(format, date.format(today));
+
+    }
+}
+
