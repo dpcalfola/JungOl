021991d10a73b0c958cd1f5c5ab5bf55e438faa7
Fola Flor <dpcalfola@gmail.com>
2021-12-30 08:55:14 +0900

    J512 J513

---
+++ src/JungOl/J513.java
@@ -0,0 +1,21 @@
+package JungOl;
+
+public class J513 {
+    public static void main(String[] args) {
+
+        double unitYd = 91.44;
+        double unniIn = 2.54;
+
+        double inputYd = 2.1 ;
+        double inputIn = 10.5 ;
+
+        double answer1 = unitYd * inputYd ;
+        double answer2 = unniIn * inputIn ;
+
+
+        System.out.printf(" %.1fyd = %.1fcm\n",inputYd, answer1);
+        System.out.printf("%.1fin =  %.1fcm\n",inputIn, answer2);
+
+
+    }
+}
