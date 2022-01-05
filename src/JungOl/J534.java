bf2bd4047b9ade95ccab6f0e74ed7e85f41612b9
Fola Flor <dpcalfola@gmail.com>
2022-01-05 09:00:02 +0900

    J515 J537 J538 J539 J555 J556 J557 J558 J559

--- src/JungOl/J534.java
+++ src/JungOl/J534.java
@@ -1,7 +1,31 @@
 package JungOl;
 
+import java.util.Scanner;
+
 public class J534 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        char c = scan.next().charAt(0);
+
+        switch (c) {
+            case 'A':
+                System.out.println("Excellent");
+                break;
+            case 'B':
+                System.out.println("Good");
+                break;
+            case 'C':
+                System.out.println("Usually");
+                break;
+            case 'D':
+                System.out.println("Effort");
+                break;
+            case 'F':
+                System.out.println("Failure");
+                break;
+            default:
+                System.out.println("error");
+        }
     }
 }
---
+++ src/JungOl/J535.java
@@ -0,0 +1,28 @@
+package JungOl;
+
+import java.util.Scanner;
+
+public class J535 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int score = (int) scan.nextDouble();
+        scan.close();
+
+        String result = "";
+
+        switch (score) {
+            case 4:
+                result = "scholarship";
+                break;
+            case 3:
+                result = "next semester";
+                break;
+            case 2:
+                result = "seasonal semester";
+                break;
+            default:
+                result = "retake";
+        }
+        System.out.println(result);
+    }
+}
