dadc101c888ee32e8406ee8d222bf7d9f60ee48b
Fola Flor <dpcalfola@gmail.com>
2022-01-06 18:12:19 +0900

    Solve
    J120 J125 J126 J521 J571 J572 J613_1

--- src/jungOl/J559.java
+++ src/jungOl/J559.java
@@ -8,6 +8,7 @@ public class J559 {
 
         Scanner scan = new Scanner(System.in);
 
+        // 입력받은 값과 배열의 인덱스를 일치시키기 위해 -1 씩 더함
         int class1 = scan.nextInt() - 1;
         int class2 = scan.nextInt() - 1;
 
--- src/jungOl/J613.java
+++ src/jungOl/J613.java
@@ -4,21 +4,39 @@ import java.util.Scanner;
 
 public class J613 {
 
-    static class student {
+//    static class student {
+//        String name;
+//        String school;
+//        int grade;
+//    }
+
+
+
         String name;
         String school;
         int grade;
-    }
+
 
 
     public static void main(String[] args) {
+
+//        class student {
+//            String name;
+//            String school;
+//            int grade;
+//        }
+
         Scanner scan = new Scanner(System.in);
 
-        student stu = new student();
 
+        J613 stu = new J613();
+
+//        student stu = new student();
+//
         stu.name = scan.next();
         stu.school = scan.next();
         stu.grade = scan.nextInt();
+
 
         System.out.println("Name : " + stu.name);
         System.out.println("School : " + stu.school);
